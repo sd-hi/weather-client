ff171f6785513bc394e6f4afbc3a6e5117e3b05a
sd-hi <sd-hi@users.noreply.github.com>
2023-07-29 22:09:17 -0400

    Device and location support

--- weather-client.py
+++ weather-client.py
@@ -1,4 +1,7 @@
+import adafruit_ahtx0
+import board
 import json
+import os
 import random
 import requests
 import time
@@ -8,8 +11,17 @@ ENV_PATH=".env"
 
 def read_env_file(path):
     # Get environment variables
-    with open(path, 'r') as file:
+    env_data = None
+    
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.join(script_directory, path)
+
+    with open(full_path, 'r') as file:
         env_data = file.read()
+
+    if env_data == None:
+        print("No .env file present with necessary parameters")
+        exit()
 
     # Split the data by lines and extract the key-value pairs
     env_vars = {}
@@ -21,14 +33,27 @@ def read_env_file(path):
 
 def get_weather():
     # Get current weather
+    
+    # Poll device for temperature and humidity
+    sensor = adafruit_ahtx0.AHTx0(board.I2C())
+
+    # Prepare data
+    sensor_id = env_vars.get("CLIENT_SENSORID")
+    location_id = env_vars.get("CLIENT_LOCATIONID")
     current_utc_timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    print(current_utc_timestamp)
+    temperature = sensor.temperature
+    humidity = sensor.relative_humidity
+
+    print(f"{current_utc_timestamp}\tTemp: {temperature}\tHumid: {humidity}")
 
     weather = {
-        'datetime': current_utc_timestamp,
-        'temperature': random.uniform(20,39),
-        'humidity': random.uniform(0.7,0.9)
+        'deviceId': sensor_id,
+        'locationId': location_id,
+        'dateTime': current_utc_timestamp,
+        'temperature': temperature,
+        'humidity': humidity
     }
+    print(weather)
 
     return weather
 
@@ -47,9 +72,9 @@ def update(env_vars):
     url = env_vars.get("SERVER_URL") + ":" + env_vars.get("SERVER_PORT") + "/weather"
     response = requests.post(url, data=json_data, headers=headers)
 
+    # Handle the response
     if response.status_code == 200:
-        print("POST request successful!")
-        print("Response:", response.json())
+        print("Response OK:", response.json())
     else:
         print("POST request failed! Status Code:", response.status_code)
         print("Error message:", response.text)
@@ -57,7 +82,14 @@ def update(env_vars):
 # Set up application
 env_vars = read_env_file(ENV_PATH)
 
+# Announce start
+update_interval = float(env_vars.get("UPDATE_INTERVAL"))
+sensor_id = env_vars.get("CLIENT_SENSORID")
+url = env_vars.get("SERVER_URL") + ":" + env_vars.get("SERVER_PORT") + "/weather"
+print(f"Sensor '{sensor_id}' started (interval {update_interval} seconds)...")
+print(url)
+
 # Start update loop
 while True:
     update(env_vars)
-    time.sleep(float(env_vars.get("UPDATE_INTERVAL")))
+    time.sleep(update_interval)
